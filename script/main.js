3294027788b758993e778ee49439e55367963a08
Dieter Randolph <dieter@Jennifers-MacBook-Pro.local>
2018-03-08 11:56:43 -0500

    basic form processor

--- script/main.js
+++ script/main.js
@@ -3,4 +3,22 @@
 
 $(function() {
 
+  $("#fileUpload").submit(function(e) {
+    var formData;
+    formData = new FormData();
+    formData.append("file", $("#fileSelector")[0].files[0]);
+    $.ajax({
+      url : 'http://localhost:8080/file/', //assumes that RMS is running on localhost:8080; adjust this accordingly.
+      data : formData,
+      processData : false,
+      type : 'PUT',
+      contentType : 'multipart/form-data',
+      mimeType: 'multipart/form-data',
+      success: function(data) {
+        console.log(data);
+      }
+    });
+    e.preventDefault();
+  });
+
 });
